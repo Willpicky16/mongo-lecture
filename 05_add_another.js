44af9ab42620b9a79060ccf0994f4215f96b045c
William Pickering <willpicky@gmail.com>
2017-03-28 10:14:31 +0100

    Add another pizza (collection)

---
+++ 05_add_another.js
@@ -0,0 +1,9 @@
+use pizza_db;
+
+db.pizzas.insert({
+    name: 'Tandori',
+    price: 15.05,
+    vegetarian: false,
+    toppings: ['Cheese', 'Tomato', 'Coriander', 'Chili', 'Chicken', 'Onions', 'Curry'],
+    customer_rating: 2
+})
